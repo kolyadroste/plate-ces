2f7a323be6c232cccf7af75d086bc2903d8ad812
Kolya von Droste zu Vischering <droste@pixelpoint.at>
2023-10-17 22:51:14 +0200

    [feature] optimize ce label

--- Resources/Private/CEs/Label/Config/tt_content.php
+++ Resources/Private/CEs/Label/Config/tt_content.php
@@ -5,8 +5,10 @@ defined('TYPO3') or die();
  * plugin Field definitions
  * */
 
-$plugin_showfields = '--palette--;;headers,--palette--;;general,
-    --div--;Text,bodytext,
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tt_content',
+	'modifiedHeaders', 'header,--linebreak--,subheader,--linebreak--, tx_plate_ce_layout, header_position, date');
+
+$plugin_showfields = '--palette--;;general,--palette--;;modifiedHeaders,
     --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
     --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames';
 
@@ -17,20 +19,61 @@ $pp_additional_columns = [
 
 
 // Configure the default backend fields for the content element
+
+// Add Content Element
 $GLOBALS['TCA']['tt_content']['types'][$plugin] = [
-    'showitem' => $plugin_showfields,
-    'columnsOverrides' => [
-        'header' => [
-            'label' => 'Bezeichner (Nur für das Typo3 Backend)'
-        ],
-        'bodytext' => [
-            'config' => [
-                'enableRichtext' => true,
-                'richtextConfiguration' => 'default'
-            ]
-        ],
-    ]
+	'showitem' => $plugin_showfields,
+	'columnsOverrides' => [
+		'header' => [
+			'label' => 'Label header',
+			'config' => [
+				'type' => 'input',
+			]
+		],
+		'tx_plate_ce_layout' => [
+			'label' => 'Layout',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'items' => [
+					[
+						'label' => 'Standard',
+						'value' => '',
+					]
+				],
+			]
+		],
+		'header_position' => [
+			'label' => 'Position',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'items' => [
+					[
+						'label' => 'Standard',
+						'value' => '',
+					],
+					[
+						'label' => 'Mittig',
+						'value' => 'center',
+					],
+					[
+						'label' => 'Rechts',
+						'value' => 'end',
+					],
+					[
+						'label' => 'Links',
+						'value' => 'start',
+					]
+				],
+			]
+		],
+	]
 ];
+
+
+
+
 
 
 
